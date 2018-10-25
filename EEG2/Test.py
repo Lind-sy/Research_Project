86d5d34dcda0f35b0859061c7650451a971ed63a
linux <Serenetija91@inbox.lv>
2018-10-25 01:36:45 -0700

    First commit

---
+++ EEG2/Test.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+import time
+
+import numpy as np
+from functools import reduce
+import math as m
+
+import scipy.io
+
+from scipy.interpolate import griddata
+from sklearn.preprocessing import scale
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.stats as scs
+import re
+
+from IPython.core.display import display, HTML
